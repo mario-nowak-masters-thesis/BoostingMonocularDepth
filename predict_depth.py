afeced923a9b400a35ce3ff2a04f86fee8e38b17
Mario Nowak <48293574+mario-nowak@users.noreply.github.com>
2023-05-01 14:30:58 +0200

    Add option to use MiDaS (#5)

--- predict_depth.py
+++ predict_depth.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     boosting_monocular_depth_pipeline = BoostingMonocularDepthPipeline(
         device=torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu"),
         pix2pix_model_path="pix2pix/checkpoints/mergemodel/latest_net_G.pth",
-        leres_model_path="res101.pth",
+        depth_estimator_model_path="res101.pth",
     )
 
     depth_prediction = boosting_monocular_depth_pipeline(input_image)
